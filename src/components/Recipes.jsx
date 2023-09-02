6a3f40c25491bade82d4fe0745ab81ff65e90bf4
Rushalee <rushaleedas@gmail.com>
2023-09-02 14:20:47 +0530

    search bar changes

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {useState} from 'react'
 import {BiSearchAlt2} from 'react-icons/bi'
 import Loading from './Loading'
 import Searchbar from './Searchbar'
+import RecipeCard from './RecipeCard'
+import { fetchRecipes } from '../utils'
 
 const Recipes = () => {
     const [recipes, setRecipes] = useState([])
@@ -14,6 +16,25 @@ const Recipes = () => {
         setQuery(e.target.value)
     }
 
+    const fetchRecipe = async() => {
+        try{
+            const data = await fetchRecipes({query, limit})
+            setRecipes(data)
+            setLoading(false)
+        }
+        catch(error)
+        {
+            console.log(error)
+        }
+        finally{
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        setLoading(true)
+        fetchRecipe()
+    }, [])
     if(loading)
     {
         return (
@@ -37,10 +58,15 @@ const Recipes = () => {
             recipes?.length > 0 ? (
                 <>
                     <div className='w-full flex flex-wrap gap-10 px-0 lg:px-10 py-10'>
-                        {}
+                        {recipes?.map((item, index) => (
+                            <RecipeCard recipes = {recipes} key ={index} />
+                        ))}
                     </div>
                 </>
-            )
+            ) : 
+            <div className='text-white w-full items-center justify-center py-10'>
+                    <p className='text-center'>No Recipe Found</p>
+                </div>
         }
     </div>
   )
